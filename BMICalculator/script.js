0ed79cf6c84c05ea5e2f69ee2b66061b5ce8ad3c
Lin Pyae Maung <linpyaemg.lpmaung@gmail.com>
2026-09-10 16:42:01 +0630

    third commit

--- BMICalculator/script.js
+++ BMICalculator/script.js
@@ -4,26 +4,25 @@ let height = document.getElementById("height").value;
 let result = document.getElementById("result");
 
 
-    if (weight <= 0 || height <= 0) {
+    if (weight == 0 || height == 0) {
         result.innerHTML = "Please enter valid weight and height.";
-       
         return;
     }
 
 let heightInMeter = height / 100;
 let bmi = weight / (heightInMeter * heightInMeter);
-bmi = bmi.toFixed(2);
+bmi=bmi.toFixed(2);
 let category;
 
 if (bmi < 18.5) {
     category = "Underweight";
-} 
-else if (bmi < 25) {
+}
+else if (bmi >= 18.5 && bmi < 25) {
     category = "Normal weight";
-} 
-else if (bmi < 30) {
+}
+else if (bmi >= 25 && bmi < 30) {
     category = "Overweight";
-} 
+}
 else {
     category = "Obese";
 }
